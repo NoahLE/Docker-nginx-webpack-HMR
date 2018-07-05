b75ab74e1e6d1e8a833fc2c609017d2132c41e33
Noah Lloyd-Edelman <nlloyd-edelman@rategenius.com>
2018-07-05 12:18:04 -0500

    Initial commit

---
+++ website/src/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>I am a sandbox app</p>
+</template>
+
+<script>
+  export default {
+    name: "App"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ website/webpack.js
@@ -0,0 +1,131 @@
+const path = require('path');
+const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  context: path.join(__dirname, 'src'),
+  entry: {
+    app: path.join(__dirname, 'src/js/app.js')
+  },
+  output: {
+    filename: 'js/app.js',
+    path: path.join(__dirname, 'dist'),
+    // publicPath: '//local.sandbox.com/',
+  },
+  resolve: {
+    extensions: ['.js', '.css', '.json', '.vue', '.jpg', '.png'],
+    modules: [
+      'node_modules'
+    ],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+    }
+  },
+  devtool: 'eval-source-map',
+  target: 'web',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    // disableHostCheck: true,
+    // headers: { 'Access-Control-Allow-Origin': '*' },
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    hot: true,
+    inline: true,
+    noInfo: false,
+    open: false,
+    port: 3000,
+    public: 'local.sandbox.com:3000'
+  },
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+          },
+          hot: true
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          process.env.NODE_ENV !== 'production' ?
+            'vue-style-loader' :
+            MiniCssExtractPlugin.loader,
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'img/[name].[ext]',
+          objectAssign: 'Object.assign'
+        },
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // template: "html/index.html",
+      title: 'Sandbox application',
+      // appEntryID: 'my-app',
+      // filename: 'index.html',
+      // favicon: 'img/favicon.png',
+      minify: true,
+      // meta: '',
+      // hash: true,
+      // cache: true
+    }),
+    new VueLoaderPlugin({
+      transformAssetUrls: {
+        video: ['src', 'poster'],
+        source: 'src',
+        img: 'src',
+        image: 'xlink:href'
+      }
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(['dist']),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ]
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map';
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
